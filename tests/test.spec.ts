59bb72e0d586f9a73cbb1ab74e265c6dc83ce57d
Łukasz <lukasznaurecki@gmail.com>
2023-12-31 16:17:13 +0100

    Implement form data filling happy path

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -36,3 +36,38 @@ test.describe('Verification tests', async () => {
         }
     });
 });
+
+test.describe('Form submission tests', async () => {
+    test('Form with correct data', async ({ page }) => {
+        await page.goto('https://ultimateqa.com/complicated-page/');
+
+        await page.locator('#et_pb_contact_name_1').waitFor({ state: 'visible' })
+        await page.locator('#et_pb_contact_name_1').click()
+        await page.fill('#et_pb_contact_name_1', 'John Doe');
+
+        await page.locator('#et_pb_contact_email_1').click()
+        await page.fill('#et_pb_contact_email_1', 'john.doe@example.com');
+
+        await page.locator('#et_pb_contact_message_1').click()
+        await page.fill('#et_pb_contact_message_1', 'This is a sample message.');
+
+        const captchaQuestion = await page.textContent('.et_pb_contact_captcha_question >> nth=1');
+        if (captchaQuestion) {
+            const numbers = captchaQuestion.match(/\d+/g)?.map(Number);
+            if (numbers) {
+                const sum = numbers.reduce((a, b) => a + b, 0);
+                await page.fill('.input.et_pb_contact_captcha >> nth=1', sum.toString());
+            }
+        }
+
+        await page.click('.et_pb_contact_submit >> nth=1');
+
+        await page.waitForSelector('.et-pb-contact-message p', {
+            state: 'attached',
+            timeout: 5000
+        });
+
+        const messageText = await page.textContent('.et-pb-contact-message p');
+        expect(messageText).toContain('Thanks for contacting us');
+    });
+})
