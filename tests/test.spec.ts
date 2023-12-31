b79b57a5756a40d4c9db2f2f4ba25a3354357e73
Łukasz <lukasznaurecki@gmail.com>
2023-12-31 16:24:57 +0100

    Implement test for form submission with missing Name field

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -70,4 +70,33 @@ test.describe('Form submission tests', async () => {
         const messageText = await page.textContent('.et-pb-contact-message p');
         expect(messageText).toContain('Thanks for contacting us');
     });
+
+    test('submit form with missing Name field', async ({ page }) => {
+        await page.goto('https://ultimateqa.com/complicated-page/');
+
+        const emailField = page.locator('#et_pb_contact_email_1');
+        await emailField.fill('john.doe@example.com');
+
+        const messageField = page.locator('#et_pb_contact_message_1');
+        await messageField.fill('This is a sample message.');
+
+        const captchaQuestion = await page.textContent('.et_pb_contact_captcha_question >> nth=1');
+        if (captchaQuestion) {
+            const numbers = captchaQuestion.match(/\d+/g)?.map(Number);
+            if (numbers && numbers.length === 2) {
+                const sum = numbers[0] + numbers[1];
+                await page.fill('.input.et_pb_contact_captcha >> nth=1', sum.toString());
+            }
+        }
+
+        await page.click('.et_pb_contact_submit >> nth=1');
+
+        await page.waitForSelector('.et-pb-contact-message p', {
+            state: 'visible',
+            timeout: 5000
+        });
+        const errorMessage = await page.textContent('.et-pb-contact-message p');
+        expect(errorMessage).toContain('Please, fill in the following fields:');
+        expect(await page.textContent('.et-pb-contact-message ul li')).toBe('Name');
+    });
 })
